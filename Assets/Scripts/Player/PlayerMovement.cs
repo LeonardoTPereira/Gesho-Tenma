9e400059107b89a8d722b0d1a53d1c19e0ffdeea
LeonardoTPereira <leonardo.t.pereira13@gmail.com>
2022-01-20 18:03:17 -0300

    Adds new Input System package. Adds player prefab with Player input, rigidbody, placeholder sprite and Movement Script. Adds Input Callback Event to PlayerMovement.cs and the logic to use the data to move the player. Adds .idea files to gitignore

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,19 +1,32 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        private Rigidbody2D _rigidbody2D;
+        private float _inputX;
+        private float _inputY;
+        [SerializeField] private float moveSpeed;
+        public float MoveSpeed => moveSpeed;
+
+        private void Start()
         {
-        
+            _rigidbody2D = GetComponent<Rigidbody2D>();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void FixedUpdate()
         {
-            //Check if player can move
+            _rigidbody2D.velocity = new Vector2(_inputX * MoveSpeed, _inputY * MoveSpeed);
+        }
+
+        public void Move(InputAction.CallbackContext context)
+        {
+            _inputX = context.ReadValue<Vector2>().x;
+            _inputY = context.ReadValue<Vector2>().y;
         }
     }
+
 }
