738ddf9493733cb2615e7d613e3b88a41a0b4cc4
LeonardoTPereira <leonardo.t.pereira13@gmail.com>
2022-01-23 15:03:44 -0300

    Implements HealthUI controller and the UI elements associated with it. Creates two event args and handlers for initializing the full health of an agent and broadcasting when it takes damage. Adds broadcasting these two events in the Player Controller. Adds sprites for the player Health UI at the left bottom corner.

---
+++ Assets/Scripts/Events/InitializeHealthEventArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace Events
+{
+    public delegate void InitializeHealthEventHandler(object sender, InitializeHealthEventArgs eventArgs);
+    public class InitializeHealthEventArgs
+    {
+        public int MaxHealth { get; }
+
+        public InitializeHealthEventArgs(int maxHealth)
+        {
+            MaxHealth = maxHealth;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,14 +8,21 @@ namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
-
+        public static event InitializeHealthEventHandler InitializePlayerHealthEventHandler;
+        public static event TakeDamageEventHandler PlayerTakeDamageEventHandler;
         [SerializeField] private bool canTakeDamage;
         [SerializeField] private int health = 5;
+        [SerializeField] private int maxHealth = 5;
         private readonly float invincibilityCooldown = 0.5f;
 
         private void Awake()
         {
             CanTakeDamage = true;
+        }
+
+        private void Start()
+        {
+            InitializePlayerHealthEventHandler?.Invoke(this, new InitializeHealthEventArgs(maxHealth));
         }
 
         private void OnEnable()
@@ -32,6 +39,7 @@ namespace Player
         {
             if (!CanTakeDamage) return;
             Health -= eventArgs.Bullet.Damage;
+            PlayerTakeDamageEventHandler?.Invoke(this, new TakeDamageEventArgs(eventArgs.Bullet.Damage));
             StartCoroutine(CountInvincibilityCooldown());
             Debug.Log($"$Player now has {Health} health points.");
         }
