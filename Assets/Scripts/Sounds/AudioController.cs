5172b6c948ce25a10aea5ad1da469d18771c9cec
LeonardoTPereira <leonardo.t.pereira13@gmail.com>
2022-01-29 23:48:56 -0300

    Adds the files that were forgotten on last commit

--- Assets/Scripts/Sounds/AudioController.cs
+++ Assets/Scripts/Sounds/AudioController.cs
@@ -1,7 +1,134 @@
-﻿namespace Sounds
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Scenes;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Sounds
 {
-    public class AudioController
+    public class AudioController : MonoBehaviour
     {
+        public struct Audio
+        {
+            public Audio(AudioSource source, Sound sound)
+            {
+                Source = source;
+                Sound = sound;
+            }
+
+            public AudioSource Source;
+            public Sound Sound;
+        }
+
+        private Dictionary<string, Audio> _audioDictionary = new Dictionary<string, Audio>();
+
+        [SerializeField] [Header("Sounds")] 
+        private List<Sound> soundEffects = new List<Sound>();
+        [SerializeField]
+        private List<Sound> bgms = new List<Sound>();
+
+        private void Awake()
+        {
+            foreach (var sound in soundEffects)
+            {
+                _audioDictionary.Add(sound.name, new Audio(AddAudioSourceFromSoundSo(sound), sound));
+            }
+            foreach (var bgm in bgms)
+            {
+                _audioDictionary.Add(bgm.name, new Audio(AddAudioSourceFromSoundSo(bgm), bgm));
+            }
+        }
+
+        private void OnEnable()
+        {
+            BattleScene.BattleStartEventHandler += PlayBGM;
+            BattleScene.BattleStartEventHandler += PlayWarning;
+        }
         
+        private void OnDisable()
+        {
+            BattleScene.BattleStartEventHandler -= PlayBGM;
+            BattleScene.BattleStartEventHandler -= PlayWarning;
+        }
+
+        private AudioSource AddAudioSourceFromSoundSo(Sound sound)
+        {
+            var newAudioSource = gameObject.AddComponent<AudioSource>();
+            newAudioSource.rolloffMode = sound.AudioMode;
+            newAudioSource.outputAudioMixerGroup = sound.Output;
+            newAudioSource.clip = sound.Clip;
+            newAudioSource.volume = sound.Volume;
+            newAudioSource.pitch = sound.Pitch;
+            newAudioSource.loop = sound.Loop;
+            newAudioSource.playOnAwake = sound.PlayOnAwake;
+            newAudioSource.spatialBlend = sound.SpatialBlend;
+            newAudioSource.maxDistance = sound.MaxRange;
+            return newAudioSource;
+        }
+        
+
+
+        private void PlayBGM(object sender, EventArgs eventArgs)
+        {
+            Debug.Log("Play BGM");
+            PlaySound(_audioDictionary["Boss1Theme"]);
+        }
+        private void PlayWarning(object sender, EventArgs eventArgs)
+        {
+            Debug.Log("Play SFX");
+            PlayAudioOnce(_audioDictionary["Warning"]);
+        }
+
+        private void PlaySound(Audio sound)
+        {
+            if (sound.Source.isPlaying)
+                return;
+            Debug.Log("Play: "+ sound.Source.clip.name);
+            sound.Source.Play();
+        }
+     
+        public void PlayAudioOnce(Audio sound)
+        {
+            if (sound.Source == null)
+                return;
+            Debug.Log("Play One Shot"+ sound.Source.clip.name);
+            sound.Source.PlayOneShot(sound.Source.clip);
+        }
+     
+        public void PlayAudioOnceRandomizePitch(AudioSource source, Sound sound)
+        {
+            if (source == null)
+                return;
+            source.pitch = (Random.Range(sound.PitchMin, sound.PitchMax));
+            source.PlayOneShot(source.clip);
+        }
+     
+        public void FadeOutAudio(AudioSource source)
+        { StartCoroutine(StartFade(source, 2, 0)); }
+     
+        public IEnumerator StartFade(AudioSource fadeSource, float duration, float targetVolume)
+        {
+            float currentTime = 0;
+            float start = fadeSource.volume;
+     
+            while (currentTime < duration)
+            {
+                currentTime += Time.deltaTime;
+                fadeSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+                yield return null;
+            }
+        }
+     
+        public void StopAudio(AudioSource source)
+        {
+            if (source == null)
+                return;
+            source.Stop();
+        }
+        
+        public List<Sound> SoundEffects => soundEffects;
+        public List<Sound> BGMs => bgms;
+
     }
 }
--- Assets/Scripts/Sounds/Sound.cs
+++ Assets/Scripts/Sounds/Sound.cs
@@ -1,31 +1,40 @@
 //Thanks to https://twitter.com/TheSarahChimera for the Script!
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
-public class Sound : ScriptableObject
+namespace Sounds
 {
-{
-    [Header("Clip and Output")]
-    public AudioClip clip = null;
-    public AudioMixerGroup output = null;
-    public bool loop = false;
-    public bool playOnAwake = false;
- 
-    [Header("Volume and Pitch")]
-    [Range(0f, 1f)]
-    public float volume = 0.5f;
-    [Range(-3f, 3f)]
-    public float pitch = 1f;
- 
-    public float pitchMin;
-    public float pitchMax;
- 
-    [Header("3D Sound")]
-    [Range(0, 1f)]
-    public float spatialBlend = 1f;
- 
-    public AudioRolloffMode audioMode;
-    public float maxRange = 3000f;
-}
+    [CreateAssetMenu(menuName = "Game Systems/Audio/Sound")]
+    public class Sound : ScriptableObject
+    {
+        [SerializeField] private float pitchMin;
+        [SerializeField] private float pitchMax;
+        [SerializeField] private float maxRange = 3000f;
+        [SerializeField] private AudioRolloffMode audioMode;
+        [SerializeField]
+        [Header("3D Sound")]
+        [Range(0, 1f)] private float spatialBlend = 1f;
+
+        [SerializeField] [Range(-3f, 3f)] private float pitch = 1f;
+        [SerializeField]
+        [Header("Volume and Pitch")]
+        [Range(0f, 1f)] private float volume = 0.5f;
+
+        [SerializeField] private bool playOnAwake;
+        [SerializeField] private bool loop;
+        [SerializeField] private AudioMixerGroup output;
+        [SerializeField] [Header("Clip and Output")] private AudioClip clip;
+
+        public AudioClip Clip => clip;
+        public AudioMixerGroup Output => output;
+        public bool Loop => loop;
+        public bool PlayOnAwake => playOnAwake;
+        public float Volume => volume;
+        public float Pitch => pitch;
+        public float PitchMin => pitchMin;
+        public float PitchMax => pitchMax;
+        public float SpatialBlend => spatialBlend;
+        public AudioRolloffMode AudioMode => audioMode;
+        public float MaxRange => maxRange;
+    }
 }
