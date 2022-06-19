0606a21c90065e39fac0897eb7e83e612549404c
LeonardoTPereira <leonardo.t.pereira13@gmail.com>
2022-06-19 16:17:48 -0300

    Adds Megaman-like Boss Selection Scene with UI prefabs and scripts to control the UI and load data when selected. Still with placeholder Boss data and sprites.

---
+++ Assets/Scripts/BossSelection/LevelData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.LevelSelection
+{
+    [Serializable]
+    [CreateAssetMenu(fileName = "LevelData", menuName = "Overlord-Project/LevelData", order = 0)]
+    public class LevelData : ScriptableObject
+    {
+        [SerializeField] private bool _completed;
+        [SerializeField] private bool _surrendered;
+
+        //TODO: Pass boss and scenario parameters
+        public void Init()
+        {
+            _completed = false;
+            _surrendered = false;
+        }
+
+        public void CompleteLevel()
+        {
+            _completed = true;
+        }
+
+        public void GiveUpLevel()
+        {
+            _surrendered = true;
+        }
+
+        public bool HasCompleted()
+        {
+            return _completed || _surrendered;
+        }
+    }
+}
---
+++ Assets/Scripts/BossSelection/LevelDescription.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game.LevelSelection
+{
+    public class LevelDescription : MonoBehaviour
+    {
+        public string DungeonDescription { get; set; }
+        public string QuestDescription { get; set; }
+
+        [field:SerializeField] public TextMeshProUGUI DisplayedText { get; set; }
+
+        private bool _isShowingDungeon;
+
+        public void CreateDescriptions(LevelData levelData)
+        {
+            _isShowingDungeon = false;
+            ChangeDescription();
+        }
+
+        public void ChangeDescription(InputAction.CallbackContext context)
+        {
+            ChangeDescription();
+        }
+
+        private void ChangeDescription()
+        {
+            _isShowingDungeon = !_isShowingDungeon;
+            DisplayedText.text = _isShowingDungeon ? DungeonDescription : QuestDescription;
+        }
+    }
+}
